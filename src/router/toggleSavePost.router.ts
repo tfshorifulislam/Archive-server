43008a298095bb591be1eed1c590b51dbfe8423b
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-24 22:15:11 +0600

    toggle save route update

--- src/router/toggleSavePost.router.ts
+++ src/router/toggleSavePost.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { toggleSavePost } from "../controller/toggleSavePost.controller";
+import { requireAuth } from "../middleware/requireAuth";
 
 const router = Router();
 
-router.post("/:postId", toggleSavePost);
+router.post("/:postId", requireAuth, toggleSavePost);
 
 export default router;
